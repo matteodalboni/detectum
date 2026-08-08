204950a08aa05a7c61ec3102b605c2242cc2a720
matteodalboni <matteo.dalboni.20@gmail.com>
2026-08-08 17:51:40 +0200

    Revise demos.

--- demo/main_qless.c
+++ demo/main_qless.c
@@ -1,47 +1,31 @@
-/* Computing the solution to the linear system "A*x = b" without
-explicitly forming the matrix Q (Q-less QR decomposition). In
-particular: A'*A*x = A'*b --> R'*Q'*Q*R*x = A'*b --> R'*R*x = A'*b.
-
-rng default;
-A = gallery('randsvd', [5, 3], 1000);
-b = ones(size(A,1),1);
-bb = A'*b;
-R = qr(A);
-x = R\(R'\bb)
-y = A\b;
-norm(x - y)
-fileID = fopen("./A.bin",'w');
-fwrite(fileID, A, 'single');
-fclose(fileID);
-*/
+// Compute the solution to the linear system A*x = b without
+// explicitly forming the matrix Q (Q-less QR decomposition):
+// A'*A*x = A'*b --> R'*Q'*Q*R*x = A'*b --> R'*R*x = A'*b.
 
 #include <stdio.h>
-#include <stdlib.h>
 #include "detectum.h"
 
 int main()
 {
-	Matrixf A = matrixf(5, 3);
-	Matrixf b = matrixf(A.rows, 1);
-	Matrixf x = matrixf(A.cols, 1);
-	FILE* A_file = fopen("../A.bin", "rb");
-	int i;
+	float A_data[] = {
+		0.358539f, -0.432372f, -0.494633f,
+		0.163691f, -0.220791f, -0.214771f,
+		-0.017439f, 0.025393f,  0.023145f,
+		-0.190184f, 0.225201f,  0.264294f,
+		-0.195261f, 0.216867f,  0.275309f
+	};
+	float b_data[] = { 1, 1, 1, 1, 1 };
+	Matrixf A, b;
+	Matrixf(x, 3, 1);
 
-	if (!A.data) return -1;
-	fread(A.data, sizeof(float), (size_t)(A.rows * A.cols), A_file);
-	fclose(A_file);
-	printf("\nA = \n"); matrixf_print(&A, "%9.4f ");
-	for (i = 0; i < b.rows; i++) at(&b, i, 0) = 1;
+	matrixf_init(&A, 5, 3, A_data, 1);
+	matrixf_init(&b, 5, 1, b_data, 1);
+	printf("\nA = \n"); matrixf_print(&A, "%9.6f ");
 	printf("\nb = \n"); matrixf_print(&b, "%9.4f ");
 	matrixf_multiply(&A, &b, &x, 1, 0, 1, 0);
 	matrixf_decomp_qr(&A, 0, 0, 0);
 	matrixf_transpose(&A); matrixf_solve_tril(&A, &x, &b, 0);
 	matrixf_transpose(&A); matrixf_solve_triu(&A, &b, &x, 0);
 	printf("\nx = \n"); matrixf_print(&x, "%9.4f ");
-
-	free(A.data);
-	free(b.data);
-	free(x.data);
-
 	return 0;
 }
--- demo/main_solve_qr.c
+++ demo/main_solve_qr.c
@@ -1,40 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "detectum.h"
 
+#define m 5
+#define n 7
 #define IN_PLACE
 
 int main()
 {
 	int i, j;
-	float work[7] = { 0 };
-	FILE* A_file = fopen("../A.bin", "rb");
-	Matrixf A = matrixf(5, 7);
+	float A_data[m * n] = {
+		 3.7688e-01f, -6.5364e-02f,  4.3145e-01f,  6.0063e-04f,  3.3769e-01f,  3.8228e-01f, -4.6315e-01f,
+		 1.0081e-01f,  1.9233e-02f,  7.4142e-03f,  1.0015e-02f, -6.3969e-03f,  2.4442e-02f, -5.0215e-02f,
+		-2.7801e-01f, -7.1722e-03f, -1.4827e-01f, -3.4716e-02f, -9.9950e-02f, -1.6848e-01f,  1.9528e-01f,
+		 5.3574e-02f, -1.3641e-02f,  7.8707e-02f,  8.9973e-04f,  6.4819e-02f,  6.5851e-02f, -7.5356e-02f,
+		 4.6850e-02f,  2.4224e-02f, -6.0995e-02f,  1.4679e-03f, -6.1765e-02f, -2.8510e-02f,  5.6545e-03f
+	};
+	float work[n] = { 0 };
+	Matrixf A;
 #ifdef IN_PLACE
-	float* data = malloc(sizeof(float) * 7 * 2);
-	Matrixf B = { A.rows, 2, data };
-	Matrixf X = { A.cols, 2, data };
+	float data[(m > n ? m : n) * 2];
+	Matrixf B = { m, 2, data };
+	Matrixf X = { n, 2, data };
 #else
-	Matrixf B = matrixf(A.rows, 2);
-	Matrixf X = matrixf(A.cols, 2);
+	Matrixf(B, m, 2);
+	Matrixf(X, n, 2);
 #endif
 
-	if (!A.data) return -1;
-	fread(A.data, sizeof(float), (size_t)(A.rows * A.cols), A_file);
-	fclose(A_file);
+	matrixf_init(&A, m, n, A_data, 1);
 	for (j = 0; j < B.cols; j++)
 		for (i = 0; i < B.rows; i++)
 			at(&B, i, j) = j + 1.0f;
-	printf("\nA = \n"); matrixf_print(&A, "%9.4f ");
-	printf("\nB = \n"); matrixf_print(&B, "%9.4f ");
+	printf("\nA = \n"); matrixf_print(&A, "%10.7f ");
+	printf("\nB = \n"); matrixf_print(&B, "%10.7f ");
 	matrixf_solve_qrp(&A, &B, &X, -1, work);
-	printf("\nX = \n"); matrixf_print(&X, "%9.4f ");
-
-	free(A.data);
-	free(B.data);
-#ifndef IN_PLACE
-	free(X.data);
-#endif
-
+	printf("\nX = \n"); matrixf_print(&X, "%10.4f ");
 	return 0;
 }
