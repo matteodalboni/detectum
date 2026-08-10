4315bf8edae37d4ae7e2c360f6adee5b53d7c9e9
matteodalboni <matteo.dalboni.20@gmail.com>
2026-08-10 14:56:10 +0200

    Improve demo about inverse iteration.

--- demo/main_inverse_iteration.c
+++ demo/main_inverse_iteration.c
@@ -1,43 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "detectum.h"
 
-static int inverse_iteration(Matrixf* A, Matrixf* v,
-	float eigval_re, float eigval_im, float* work, int iter)
+int matrixf_get_eigenvector_it(Matrixf* A, Matrixf* v,
+	float lamre, float lamim, int iter, float* work)
 {
 	int i, j;
 	const int n = A->rows;
-	float norm;
-	Matrixf perm = { 2 * n, 1, work };
-	Matrixf C = { 2 * n, 2 * n, work + 2 * n };
+	const int p = lamim == 0 ? 1 : 2;
+	const int q = v->cols;
+	float norm, eps = epsf(lamre);
+	Matrixf perm = { p * n, 1, work };
+	Matrixf C = { p * n, p * n, work + p * n };
 
+	if (A->cols != n || q > 2 ||
+		v->rows != n || q < p) {
+		return -1;
+	}
 	for (j = 0; j < n; j++) {
 		for (i = 0; i < n; i++) {
 			at(&C, i, j) = at(A, i, j);
-			at(&C, i + n, j + n) = at(A, i, j);
-			at(&C, i + n, j) = 0;
-			at(&C, i, j + n) = 0;
+			if (lamim != 0) {
+				at(&C, i + n, j + n) = at(A, i, j);
+				at(&C, i + n, j) = 0;
+				at(&C, i, j + n) = 0;
+			}
 		}
-		at(&C, j, j) -= eigval_re;
-		at(&C, j + n, j + n) -= eigval_re;
-		at(&C, j + n, j) = -eigval_im;
-		at(&C, j, j + n) = +eigval_im;
-		v->data[j] = 1;
-		v->data[j + n] = 0;
+		at(&C, j, j) -= lamre + eps;
+		if (lamim != 0) {
+			at(&C, j + n, j + n) -= lamre + eps;
+			at(&C, j + n, j) = -lamim;
+			at(&C, j, j + n) = +lamim;
+		}
+		else if (q == 2) {
+			v->data[j + n] = 0;
+		}
 	}
-	v->rows = 2 * n;
+	v->rows = p * n;
 	v->cols = 1;
 	matrixf_decomp_lu(&C, &perm, 0);
 	for (i = 0; i < iter; i++) {
 		matrixf_permute(v, &perm, 0, 0);
 		matrixf_solve_tril(&C, v, v, 1);
 		matrixf_solve_triu(&C, v, v, 0);
-		norm = normf(v->data, 2 * n, 1);
-		for (j = 0; j < 2 * n; j++) {
+		norm = normf(v->data, p * n, 1);
+		for (j = 0; j < p * n; j++) {
 			v->data[j] /= norm;
 		}
 	}
 	v->rows = n;
-	v->cols = 2;
+	v->cols = q;
 	return 0;
 }
 
@@ -49,10 +61,17 @@ int main()
 	float eigval_re = 0, eigval_im = 0;
 	float work[4 * n * n + 2 * n] = { 0 };
 	float A_data[] = {
+#if 1
 		 1, 1, 1, 3,
 		 1, 2, 1, 1,
 		 1, 1, 3, 1,
 		-2, 1, 1, 4
+#else // defective matrix
+		3, 1, 0, 0,
+		0, 3, 1, 0,
+		0, 0, 3, 1,
+		0, 0, 0, 3
+#endif
 	};
 	Matrixf A;
 	Matrixf(T, n, n);
@@ -63,7 +82,6 @@ int main()
 	matrixf_decomp_schur(&T, 0);
 	printf("\nA = [\n"); matrixf_print(&A, "%9.4f "); printf("];\n");
 	printf("\nT = [\n"); matrixf_print(&T, "%9.4f "); printf("];\n");
-
 	for (k = 0; k < n; k++) {
 		eigval_re = at(&T, k, k);
 		eigval_im = 0;
@@ -71,13 +89,15 @@ int main()
 			eigval_im = -sqrtf(-at(&T, k - 1, k) * at(&T, k, k - 1));
 		if (k < n - 1 && at(&T, k + 1, k) != 0)
 			eigval_im = +sqrtf(-at(&T, k + 1, k) * at(&T, k, k + 1));
-		inverse_iteration(&A, &v, eigval_re, eigval_im, work, 2);
+		for (i = 0; i < 2 * n; i++) {
+			v.data[i] = (float)rand() / (float)RAND_MAX;
+		}
+		matrixf_get_eigenvector_it(&A, &v, eigval_re, eigval_im, 2, work);
 		printf("\neigval(:,%d) = %0.4f%+.4fi;\neigvec(:,%d) = [\n",
 			k + 1, eigval_re, eigval_im, k + 1);
 		for (i = 0; i < n; i++)
 			printf("   %9.4f%+.4fi\n", v.data[i], v.data[i + n]);
 		printf("];\n");
 	}
-
 	return 0;
 }
